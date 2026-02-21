fe7f27ac9b1b3300aa90b4702e834ccf4c0faa45
MuhammadSarimUmer <muhammadsarim426426@gmail.com>
2026-02-22 00:43:26 +0500

    new

---
+++ tuts/functions.py
@@ -0,0 +1,25 @@
+# import math
+# def factorial_number(number):
+#     return math.factorial(number)
+
+
+# result = factorial_number(5)
+# print(result)
+
+def add(a,b):
+    return a+b
+
+def substract(a,b):
+    return a-b
+
+
+def multiply(a,b):
+    return a*b
+
+
+def division(a,b):
+    if(b==0):
+        print("Division not possible with 0")
+        
+    else:
+        return a/b
---
+++ tuts/imports.py
@@ -0,0 +1,9 @@
+import functions as maths
+
+a = 5
+b = 2
+
+print("Addition: " ,maths.add(a,b))
+print("Substraction: ",maths.substract(a,b))
+print("Multiply: ",maths.multiply(a,b))
+print("Divide: ",maths.division(a,b))
