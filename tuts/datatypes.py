5b53c7d1aa9b68e6001aaf143c77b8662d0a0276
MuhammadSarimUmer <muhammadsarim426426@gmail.com>
2026-02-21 17:45:30 +0500

    Initial

---
+++ tuts/datatypes.py
@@ -0,0 +1,19 @@
+#Integer and float
+age = 25
+height = 2.9
+#String
+name = "Sarim Umer"
+greeting = "Hello" + name
+#List and tuple
+numbers = [1,2,3,4,5,6]
+names = ["Sarim","Muhammad","Umer"]
+tuple1 = (10,20) ##tuples are immutable
+
+#Dictionary 
+
+info = {"name":"Sarim", "age":25}#Like Maps
+
+
+#booleans
+
+is_student = True
